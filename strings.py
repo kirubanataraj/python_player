3da258925ebfa6f17dc3a6aa83423a2613430d16
kirubanataraj <32951248+kirubanataraj@users.noreply.github.com>
2017-10-20 16:21:41 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse020
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse020 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    string="hi hello all"
+    print(string)
+
+if __name__ == '__main__':
+    main()
