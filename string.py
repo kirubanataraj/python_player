8ce7d8de0060bea9e905870dad21dc9ea5dc31ad
kiruba <32951248+kirubanataraj@users.noreply.github.com>
2017-10-21 10:26:56 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse020
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse020 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    stringVar = "abcdefghijkxxxxxxxabcedf"
+    loc = stringVar.count('x') #counts the number of occurrences of 'x' in stringVar
+    print loc
+    loc = stringVar.find('x') # returns the position of character 'x'
+    print loc
+    stringVar = stringVar.lower() # returns the stringVar in lowercase (this is temporary)
+    print stringVar
+    stringVar = stringVar.upper() # returns the stringVar in uppercase(this is temporary)
+    print stringVar
+    stringVar = stringVar.replace('a', 'b') # replaces all occurrences of a with b in the string
+    print stringVar
+    stringVar = stringVar.strip() # removes leading/trailing white space from string
+    print stringVar
+if __name__ == '__main__':
+    main()
