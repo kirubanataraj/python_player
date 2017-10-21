545b2c074842777db8234e9928870f9af69cb1c8
kiruba <32951248+kirubanataraj@users.noreply.github.com>
2017-10-21 15:57:17 +0530

    Add files via upload

---
+++ conditionalif.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse020
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse020 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+x =1
+if x < 0:
+    x = 0
+    print('Negative changed to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
